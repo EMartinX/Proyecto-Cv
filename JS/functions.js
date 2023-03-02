1c6d84b9bf9a25d26f894a88e3dec78b502f8a58
Emanuel Martin <e.m4rt1n025@gmail.com>
2023-03-01 22:35:14 -0300

    Commit del Proyecto

---
+++ JS/functions.js
@@ -0,0 +1,25 @@
+function ocultarTrabajo() {
+  var parrafo = document.getElementById("infoTrabajo");
+  if (parrafo.style.display === "none") {
+    parrafo.style.display = "block";
+  } else {
+    parrafo.style.display = "none";
+  }
+}
+
+function ocultarEducacion() {
+  var parrafo = document.getElementById("infoEducacion");
+  if (parrafo.style.display === "none") {
+    parrafo.style.display = "block";
+  } else {
+    parrafo.style.display = "none";
+  }
+}
+function ocultarHabilidad() {
+  var parrafo = document.getElementById("infoHabilidad");
+  if (parrafo.style.display === "none") {
+    parrafo.style.display = "block";
+  } else {
+    parrafo.style.display = "none";
+  }
+}
